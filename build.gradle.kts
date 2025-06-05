58541b8a733637f5f68b2e8fab98ac52575189bb
mihai <mihai.iordache82@gmail.com>
2025-06-05 22:35:07 +0300

    finalizing library generator

---
+++ build.gradle.kts
@@ -0,0 +1,163 @@
+import java.time.LocalDateTime
+
+// ./gradlew publish -PopenApiFileName="car-module-openapi" run this command for car-module-openapi.yaml file
+// ./gradlew openApiGenerate -PopenApiFileName="car-module-openapi" just to run the openApiGenerate with variables
+// how to deploy to gitlab: https://docs.gitlab.com/user/packages/maven_repository/?tab=%60gradle%60
+
+plugins {
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.13.0"            // used to generate the classes with openApiGenerate function
+    id("java-library")                                      // we generate a java library
+    `maven-publish`                                    // instruct gradle that this is a library that needs to be pushed
+}
+
+//GITHUB --------------------
+//publishing {
+//    repositories {
+//        maven {
+//            name = "GitHubPackages"
+//            url = uri("https://maven.pkg.github.com/SuperGini/openapi-generator-lib")
+//            credentials {
+//                username = project.findProperty("user") as String? ?: System.getenv("USERNAME")
+//                password = project.findProperty("token") as String? ?: System.getenv("TOKEN")
+//            }
+//        }
+//    }
+//    publications {
+//        register<MavenPublication>("gpr") {
+//            from(components["java"])
+//        }
+//    }
+//}
+
+//GITLAB--------------------------
+publishing {
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://gitlab.com/api/v4/projects/70539492/packages/maven")
+            credentials(HttpHeaderCredentials::class) {
+                name = "Private-Token" //accepted values: Private-Token, Deploy-Token, Job-Token
+                value =
+                    findProperty("gitLabPrivateToken") as String? // the variable resides in $GRADLE_USER_HOME/gradle.properties
+            }
+            authentication {
+                create("header", HttpHeaderAuthentication::class)
+            }
+        }
+    }
+}
+
+group = "com.gini"  // groupId of library
+version = "1.0.15"  //version of the library
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21) // can only be used in projects with java 21 of higher
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+// this are the minimum dependencies to build the library
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //need this dependencies for openapi generator
+    implementation("io.swagger.core.v3:swagger-annotations-jakarta:2.2.30")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+}
+
+
+val openApiFileName: String = project.findProperty("openApiFileName") as String?
+    ?: "default" // get th filename from the openApiFileName variable. This value will be passed when we run the gitlab ci/cd
+
+// Construct the full path using the dynamic filename
+val openApiSpecPath: String =
+    "$projectDir/openapi/$openApiFileName.yaml" //this will get the openapi filename from the root project ->
+
+
+// configuration properties: https://openapi-generator.tech/docs/generators/spring/
+openApiGenerate {
+    generatorName.set("spring") // Use the Spring generator.
+    inputSpec.set(openApiSpecPath) // Path to your OpenAPI specification file.
+    outputDir.set("$rootDir/generated") // Output directory for the generated code.
+    apiPackage.set("com.gini.${openApiFileName}.api") // Package for the generated API classes.
+    modelPackage.set("com.gini.${openApiFileName}.model") // Package for the generated model classes.
+    verbose.set(true)
+    globalProperties.set(
+        mapOf(
+            "apis" to "",   //generate only the interfaces for the controlers
+            "models" to ""  //generate the models/classes that will be neded in the controllers
+        )
+    )
+    configOptions.set(
+        mapOf(
+            // Optional:  Add these to avoid a few common codegen issues.
+            "interfaceOnly" to "true",              // Generate interfaces instead of classes for controllers.
+            "useSpringBoot3" to "true",             // Ensure compatibility with Spring Boot 3. It will automatically set "useJakartaEe" to "true"
+            "useJakartaEe" to "true",               // Use Jakarta EE
+            "dateLibrary" to "java8",
+            "useTags" to "true",                    //generate a interface for all controllers under that tag
+            "skipDefaultInterface" to "true",       //do not create default interface
+            "useResponseEntity" to "false",         // don't use ResponseEntity
+//            "hideGenerationTimestamp" to "true",  //does not generate a date when the calsse wer created
+//            "modelPackage" to "com.gini.model",     //where to generate the model classses
+//            "apiPackage" to "com.gini.api",         //where to create the controller interfaces
+//            "artifactId" to "com.gini.test.artifact",
+//            "groupId" to "com.gini.group",
+        )
+    )
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("$rootDir/generated/src/main/java") // Adjust path if needed -> need this to include the classes in the library
+        }
+    }
+}
+
+tasks.named("compileJava") {
+    dependsOn("openApiGenerate") // when run ./gradlew publish it will build the library but before that it will run the openApiGenerate function
+}
+
+tasks.jar {
+    archiveClassifier.set("generated") //adds a classifier when generating the library. If this code is commented the default value will be -> plain This value can be anything i chose -> generated
+    manifest { // to publish some information in the META-iNF file of the library. Is optional and you can delete it if you want
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Build-By" to System.getProperty("user.name"),
+                "Build-Time" to LocalDateTime.now().toString(),
+                "Created-By" to "Gradle ${gradle.gradleVersion}",
+                "Build-Jdk" to "${System.getProperty("java.runtime.version")} (${System.getProperty("java.vendor")}) (${
+                    System.getProperty(
+                        "java.jvm.version"
+                    )
+                })",
+                "Build-OS" to "${System.getProperty("os.name")} (${System.getProperty("os.arch")}) (${
+                    System.getProperty(
+                        "os.version"
+                    )
+                })",
+
+                )
+        )
+    }
+}
+
+tasks.bootJar {
+    enabled = false // Disable the bootJar task as this is a library, not an executable application but a library
+}
+
