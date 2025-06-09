d6bf2fe171059d7eae023618c000c61460fe1523
mihai iordache <mihai.iordache82@gmail.com>
2025-06-09 23:21:33 +0300

    ff

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ publishing {
         maven {
             url = uri("https://gitlab.com/api/v4/projects/70539492/packages/maven")
             credentials(HttpHeaderCredentials::class) {
-                name = "Deploy-Token" //accepted values: Private-Token, Deploy-Token, Job-Token
+                name = "Job-Token" //accepted values: Private-Token, Deploy-Token, Job-Token
                 value = System.getenv("CI_JOB_TOKEN")
 
                 println("TOKEN_VALUE: $value +++++++++++++++++++++++++++++++++++++++++++++++++++++++")
